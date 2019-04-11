cbe5e4c064395539df7ede75978e6e18a2d42194
Phaedra <phaedra.curlin@outlook.fr>
2019-04-11 17:02:21 -0600

    added some of the commands

--- battleship.h
+++ battleship.h
@@ -13,3 +13,41 @@ struct _board{
   int submarine;
 };
 
+void printUsage();
+void help();
+void instructions();
+void restart();
+int parseArgs(int argc, char **argv,int* board_size,int *max_shots);
+
+int parseArgs(int argc, char **argv,int* board_size,int *max_shots){
+  if (argc  > 3)
+    {
+      printf("I did not understand your input. Please type again\n");
+      printUsage();
+      return -1;
+    }
+  if(argc > 1){
+    if(strcmp("-h",argv[1])==0)
+      {
+	help();
+	return -1;
+      }
+       if(strcmp("-x",argv[1])==0)
+      {
+	exit();
+      }
+       if(strcmp("-i",argv[1])==0)
+	 {
+	   instructions();
+	 }
+       return -1;
+  }
+  }	
+}
+
+board * randomFleetArrangement (board * blank_board);
+
+void printUsage() {
+  printf("Usage: Number of shots: \n Input a number between 12 and 63.\n");
+}
+
